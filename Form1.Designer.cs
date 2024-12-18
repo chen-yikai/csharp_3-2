d03ea6e2e71ce0bef45b2ec5fb359ac5127c702a
chen-yikai <eliaschen.dev@gmail.com>
2024-12-18 11:49:06 +0800

    finish!!!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.box = new System.Windows.Forms.PictureBox();
             this.up = new System.Windows.Forms.Button();
@@ -36,7 +37,16 @@
             this.down = new System.Windows.Forms.Button();
             this.right = new System.Windows.Forms.Button();
             this.pause = new System.Windows.Forms.Button();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.speedTracke = new System.Windows.Forms.TrackBar();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.horizontalS = new System.Windows.Forms.HScrollBar();
+            this.verticalS = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTracke)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer
@@ -45,10 +55,12 @@
             // 
             // box
             // 
-            this.box.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.box.BackColor = System.Drawing.Color.Transparent;
+            this.box.Image = global::csharp_3_2.Properties.Resources.arrow;
             this.box.Location = new System.Drawing.Point(61, 43);
             this.box.Name = "box";
-            this.box.Size = new System.Drawing.Size(100, 100);
+            this.box.Size = new System.Drawing.Size(200, 100);
+            this.box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.box.TabIndex = 0;
             this.box.TabStop = false;
             // 
@@ -107,11 +119,79 @@
             this.pause.UseVisualStyleBackColor = true;
             this.pause.Click += new System.EventHandler(this.pause_Click);
             // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "arrow-up.png");
+            this.imgList.Images.SetKeyName(1, "arrow-down.png");
+            this.imgList.Images.SetKeyName(2, "arrow-left.png");
+            this.imgList.Images.SetKeyName(3, "arrow-right.png");
+            // 
+            // speedTracke
+            // 
+            this.speedTracke.Location = new System.Drawing.Point(530, 261);
+            this.speedTracke.Maximum = 1000;
+            this.speedTracke.Minimum = 1;
+            this.speedTracke.Name = "speedTracke";
+            this.speedTracke.Size = new System.Drawing.Size(258, 45);
+            this.speedTracke.TabIndex = 6;
+            this.speedTracke.TickFrequency = 50;
+            this.speedTracke.Value = 500;
+            this.speedTracke.Scroll += new System.EventHandler(this.speedTracke_Scroll);
+            // 
+            // horizontalS
+            // 
+            this.horizontalS.Location = new System.Drawing.Point(47, 418);
+            this.horizontalS.Maximum = 300;
+            this.horizontalS.Minimum = 100;
+            this.horizontalS.Name = "horizontalS";
+            this.horizontalS.Size = new System.Drawing.Size(143, 20);
+            this.horizontalS.TabIndex = 7;
+            this.horizontalS.Value = 200;
+            this.horizontalS.Scroll += new System.Windows.Forms.ScrollEventHandler(this.horizontalS_Scroll);
+            // 
+            // verticalS
+            // 
+            this.verticalS.Location = new System.Drawing.Point(26, 289);
+            this.verticalS.Maximum = 300;
+            this.verticalS.Minimum = 50;
+            this.verticalS.Name = "verticalS";
+            this.verticalS.Size = new System.Drawing.Size(21, 129);
+            this.verticalS.TabIndex = 8;
+            this.verticalS.Value = 100;
+            this.verticalS.Scroll += new System.Windows.Forms.ScrollEventHandler(this.verticalS_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(50, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "大小";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(526, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "速度(快~慢)";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.verticalS);
+            this.Controls.Add(this.horizontalS);
+            this.Controls.Add(this.speedTracke);
             this.Controls.Add(this.pause);
             this.Controls.Add(this.right);
             this.Controls.Add(this.down);
@@ -121,7 +201,10 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedTracke)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,6 +217,13 @@
         private System.Windows.Forms.Button down;
         private System.Windows.Forms.Button right;
         private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.TrackBar speedTracke;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.HScrollBar horizontalS;
+        private System.Windows.Forms.VScrollBar verticalS;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace csharp_3_2
 {
@@ -19,7 +20,8 @@ namespace csharp_3_2
             switch (direction)
             {
                 case "up":
-                    if (box.Location.Y < 0 - box.Height)
+                    box.Image = imgList.Images[0];
+                    if (box.Location.Y <  - box.Height)
                     {
                         box.Location = new Point(box.Location.X, this.Height);
                     }
@@ -29,6 +31,7 @@ namespace csharp_3_2
                     }
                     break;
                 case "down":
+                    box.Image = imgList.Images[1];
                     if (box.Location.Y > this.Height)
                     {
                         box.Location = new Point(box.Location.X, 0 - box.Height);
@@ -39,6 +42,7 @@ namespace csharp_3_2
                     }
                     break;
                 case "left":
+                    box.Image = imgList.Images[2];
                     if (box.Location.X + box.Width < 0)
                     {
                         box.Location = new Point(this.Width, box.Location.Y);
@@ -49,6 +53,7 @@ namespace csharp_3_2
                     }
                     break;
                 case "right":
+                    box.Image = imgList.Images[3];
                     if (box.Location.X > this.Width)
                     {
                         box.Location = new Point(0 - box.Width, box.Location.Y);
@@ -58,7 +63,6 @@ namespace csharp_3_2
                         box.Left += space;
                     }
                     break;
-
             }
 
         }
@@ -90,6 +94,22 @@ namespace csharp_3_2
         private void pause_Click(object sender, EventArgs e)
         {
             timer.Stop();
+        }
+
+        private void speedTracke_Scroll(object sender, EventArgs e)
+        {
+            timer.Interval = speedTracke.Value;
+            //speedTracke.Value;
+        }
+
+        private void verticalS_Scroll(object sender, ScrollEventArgs e)
+        {
+            box.Height = verticalS.Value;
+        }
+
+        private void horizontalS_Scroll(object sender, ScrollEventArgs e)
+        {
+            box.Width = horizontalS.Value;
         }
     }
 }
