8d890628b40bbe0074446a058d48428e5088ca9b
chen-yikai <eliaschen.dev@gmail.com>
2024-12-18 10:54:17 +0800

    add basic direction control

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.box = new System.Windows.Forms.PictureBox();
+            this.up = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,11 +52,71 @@
             this.box.TabIndex = 0;
             this.box.TabStop = false;
             // 
+            // up
+            // 
+            this.up.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.up.Location = new System.Drawing.Point(662, 312);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(60, 60);
+            this.up.TabIndex = 1;
+            this.up.Text = "上";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // left
+            // 
+            this.left.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.left.Location = new System.Drawing.Point(596, 378);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(60, 60);
+            this.left.TabIndex = 2;
+            this.left.Text = "左";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // down
+            // 
+            this.down.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.down.Location = new System.Drawing.Point(662, 378);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(60, 60);
+            this.down.TabIndex = 3;
+            this.down.Text = "下";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // right
+            // 
+            this.right.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.right.Location = new System.Drawing.Point(728, 378);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(60, 60);
+            this.right.TabIndex = 4;
+            this.right.Text = "右";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // pause
+            // 
+            this.pause.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.pause.Location = new System.Drawing.Point(530, 378);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(60, 60);
+            this.pause.TabIndex = 5;
+            this.pause.Text = "停止";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.down);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.up);
             this.Controls.Add(this.box);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -64,6 +129,11 @@
 
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.PictureBox box;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Button pause;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,33 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace csharp_3_2
 {
     public partial class Form1 : Form
     {
+        private string direction = "";
         public Form1()
         {
             InitializeComponent();
-            timer.Start();
+
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            if(box.Location.X == this.Width)
+            int space = 10;
+            switch (direction)
             {
-                box.Location = new Point(0,box.Location.Y);
+                case "up":
+                    if (box.Location.Y < 0 - box.Height)
+                    {
+                        box.Location = new Point(box.Location.X, this.Height);
+                    }
+                    else
+                    {
+                        box.Top -= space;
+                    }
+                    break;
+                case "down":
+                    if (box.Location.Y > this.Height)
+                    {
+                        box.Location = new Point(box.Location.X, 0 - box.Height);
+                    }
+                    else
+                    {
+                        box.Top += space;
+                    }
+                    break;
+                case "left":
+                    if (box.Location.X + box.Width < 0)
+                    {
+                        box.Location = new Point(this.Width, box.Location.Y);
+                    }
+                    else
+                    {
+                        box.Left -= space;
+                    }
+                    break;
+                case "right":
+                    if (box.Location.X > this.Width)
+                    {
+                        box.Location = new Point(0 - box.Width, box.Location.Y);
+                    }
+                    else
+                    {
+                        box.Left += space;
+                    }
+                    break;
+
             }
-            else
-            {
-                box.Left += 10;
-            }
+
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            direction = "up";
+            timer.Start();
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+            direction = "left";
+            timer.Start();
+        }
+
+        private void down_Click(object sender, EventArgs e)
+        {
+            direction = "down";
+            timer.Start();
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+            direction = "right";
+            timer.Start();
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
         }
     }
 }
